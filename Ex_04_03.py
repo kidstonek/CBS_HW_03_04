1fd8e6bf0677fd9b5a1b5a92ec4fe1f5915b4593
ava <kidstonek@gmail.com>
2024-09-15 09:55:07 +0300

    +3 Ex

--- Ex_04_03.py
+++ Ex_04_03.py
@@ -9,12 +9,37 @@
 """
 
 
+class EmployeeYearError(Exception):
+    pass
+
+
 class Employee:
+    list_of_employees = []
+
     def __init__(self, name: str, surname: str, department: str, start_work_year: int):
         self.name = name
         self.surname = surname
         self.department = department
-        self.start_work_year = start_work_year
+        if isinstance(start_work_year, int):
+            self.start_work_year = start_work_year
+        else:
+            raise EmployeeYearError(f'Employee {self.name} from {self.department} department have a problem with year '
+                                    f'it is not int')
+        self.list_of_employees.append([self.name, self.surname, self.department, self.start_work_year])
 
 
-person1 = Employee('And', )
+if __name__ == '__main__':
+    my_test_employees = [['John', 'Cirby', 'IT', '2021'], ['Valera', 'Robinson', 'Log', 1021], ['Dima', 'Johnson', 'Fin', 1991]]
+    employees_ = ['person1', 'person2', 'person3']
+
+    for employee, name_of_employee in zip(my_test_employees, employees_):
+        try:
+            name_of_employee = Employee(*employee)
+        except EmployeeYearError as e:
+            print(e)
+
+    year_to_search = int(input('\nwhat year we a looking for: '))
+
+    for employee in Employee.list_of_employees:
+        if employee[3] > year_to_search:
+            print(employee)
